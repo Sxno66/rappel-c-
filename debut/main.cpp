5a43836f2e130eae1734552fe81b3d6588b1eca0
Sxno66 <sanoafben@gmail.com>
2024-09-05 12:24:59 +0200

    maj fin de day1

--- debut/main.cpp
+++ debut/main.cpp
@@ -1,5 +1,14 @@
-#include 
+#include "mission.h"
+#include "Entreprise.h"
+#include "Entreprise2.cpp"
+#include "mission.cpp"
+
+
 
 using namespace std;
 
-int main();
+int main() {
+	cout << "Hello World!";
+	return 0;
+}
+
